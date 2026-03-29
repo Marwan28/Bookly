898d0cb7ebaa9803fd21305a61510e3d59753908
MARWAN-PC\Marwan <marwanabdelwahab28@gmail.com>
2026-03-29 22:27:53 +0200

    add font family to the app

--- lib/core/utils/my_styles.dart
+++ lib/core/utils/my_styles.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 abstract class MyStyles {
   static const titleMedium = TextStyle(
     fontWeight: FontWeight.w600,
-    fontSize: 20,
+    fontSize: 18,
   );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:bookly/core/constants.dart';
 import 'package:bookly/features/splash/presentation/views/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const BooklyApp());
@@ -17,6 +18,7 @@ class BooklyApp extends StatelessWidget {
       title: 'Bookly',
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: kPrimaryColor,
+        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
       ),
       home: SplashScreen(),
     );
