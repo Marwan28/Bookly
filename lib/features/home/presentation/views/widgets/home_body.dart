a9dff317c72a3ba82563032c6606f6e1bb37a888
MARWAN-PC\Marwan <marwanabdelwahab28@gmail.com>
2026-03-30 16:47:14 +0200

    create app styles

--- lib/features/home/presentation/views/widgets/home_body.dart
+++ lib/features/home/presentation/views/widgets/home_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/core/utils/assets_data.dart';
 import 'package:bookly/core/utils/my_styles.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly/features/home/presentation/views/widgets/featured_list_view.dart';
@@ -16,7 +17,37 @@ class HomeBody extends StatelessWidget {
           CustomAppBar(),
           FeaturedListView(),
           const SizedBox(height: 50),
-          Text('Best Seller', style: MyStyles.titleMedium),
+          Text('Best Seller', style: MyStyles.textStyle18),
+          const BestSellerListViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.red,
+                image: DecorationImage(
+                  image: AssetImage(AssetsData.testImage),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          ),
+          Column(),
         ],
       ),
     );
