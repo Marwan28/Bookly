a2eb0cc1b3f808f6c62eadfe094cdfb04451c4c8
MARWAN-PC\Marwan <marwanabdelwahab28@gmail.com>
2026-04-02 21:26:27 +0200

    create home repo

---
+++ lib/features/home/data/repos/home_repo_imp.dart
@@ -0,0 +1,15 @@
+import 'package:bookly/features/home/data/repos/home_repo.dart';
+
+class HomeRepoImp extends HomeRepo{
+  @override
+  fetchBestSellerBooks() {
+    // TODO: implement fetchBestSellerBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  fetchFeaturedBooks() {
+    // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  }
+}
