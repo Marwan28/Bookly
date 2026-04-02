4f6f7273c2bb4e51a0601f2ed6604ba4c27bc0ba
MARWAN-PC\Marwan <marwanabdelwahab28@gmail.com>
2026-04-03 00:03:49 +0200

    create api service

--- lib/features/home/data/repos/home_repo_imp.dart
+++ lib/features/home/data/repos/home_repo_imp.dart
@@ -1,5 +1,18 @@
+import 'package:bookly/core/errors/failure.dart';
+import 'package:bookly/features/home/data/model/book_model/book_model.dart';
 import 'package:bookly/features/home/data/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
 
-class HomeRepoImp extends HomeRepo{
+class HomeRepoImp extends HomeRepo {
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
+    // TODO: implement fetchBestSellerBooks
+    throw UnimplementedError();
+  }
 
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
+    // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  }
 }
