1320b12cc63fb61664efa52f1e39998711f37e16
MARWAN-PC\Marwan <marwanabdelwahab28@gmail.com>
2026-04-11 17:37:58 +0200

    create newest books cubit

---
+++ lib/features/home/presentation/view_models/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,20 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly/features/home/data/model/book_model/book_model.dart';
+import 'package:bookly/features/home/data/repos/home_repo.dart';
+import 'package:meta/meta.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksLoading());
+    var result = await homeRepo.fetchNewestBooks();
+    result.fold(
+      (failure) => emit(NewestBooksError(failure.errMessage)),
+      (books) => emit(NewestBooksSuccess(books)),
+    );
+  }
+}
